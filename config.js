32fd1800dc3f04151c85d1c815cd904ee1fcf8d3
Rachelle Lee <rachedeveloper@gmail.com>
2021-04-28 20:03:44 -0700

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
   secretKey: '12345-67890-09876-54321',
   mongoUrl: 'mongodb://localhost:27017/nucampsite',
+  facebook: {
+        clientId: '733409600674796',
+        clientSecret: 'cae173ce9263d6de542432d72ce9becd'
+  }
 };
